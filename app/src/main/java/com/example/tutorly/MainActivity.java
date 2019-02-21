13a6f2acd3bb61e7c9d713498513a4dd6d664c93
shoffman2 <shoffman2@crimson.ua.edu>
2019-02-21 12:49:00 -0600

    Temporary main screen

--- app/src/main/java/com/example/tutorly/MainActivity.java
+++ app/src/main/java/com/example/tutorly/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tutorly;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+
+        Button btn = (Button)findViewById(R.id.ProfileBtn);
+        Button btn2 = (Button)findViewById(R.id.AccountBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, profileActivity.class));
+                }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, AccountActivity.class));
+                }
+        });
     }
 }
