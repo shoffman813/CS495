c50c35ead3be1a9e5c4edc313fe920ba55c69c47
evkimmel <evkimmel@crimson.ua.edu>
2019-02-20 19:14:33 -0600

    Pushing updated MainActivity, which links to the LoginActivity

--- app/src/main/java/com/example/tutorly/MainActivity.java
+++ app/src/main/java/com/example/tutorly/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.tutorly;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
