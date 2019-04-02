a9bc09722302999e6765e9b988728330a8adff40
shoffman2 <shoffman2@crimson.ua.edu>
2019-04-02 01:56:47 -0500

    adding bottom navigation to home screen

--- app/src/main/java/com/example/tutorly/MainActivity.java
+++ app/src/main/java/com/example/tutorly/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tutorly;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = (Button)findViewById(R.id.ProfileBtn);
         Button btn2 = (Button)findViewById(R.id.AccountBtn);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
         btn.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -32,5 +36,31 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(new Intent(MainActivity.this, AccountActivity.class));
                 }
         });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.profile_item:
+                                startActivity(new Intent(MainActivity.this, profileActivity.class));
+                                break;
+                            case R.id.settings_item:
+                                startActivity(new Intent(MainActivity.this, AccountActivity.class));
+                                break;
+                            case R.id.home_item:
+                                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                                break;
+                            case R.id.scheduled_item:
+                                startActivity(new Intent(MainActivity.this, SessionsScheduledActivity.class));
+                                break;
+                            case R.id.requested_item:
+                                startActivity(new Intent(MainActivity.this, SessionRequestsActivity.class));
+                                break;
+                        }
+                        return true;
+                    }
+                });
+
     }
 }
