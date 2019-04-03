99bd5723afed0aa594516c89e441d0c8d672f080
shoffman2 <shoffman2@crimson.ua.edu>
2019-04-03 16:53:34 -0500

    adding bottom nav to profileActivity

--- app/src/main/java/com/example/tutorly/profileActivity.java
+++ app/src/main/java/com/example/tutorly/profileActivity.java
@@ -1,7 +1,11 @@
 package com.example.tutorly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -42,5 +46,33 @@ public class profileActivity extends AppCompatActivity {
 
             }
         });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.profile_item:
+                                startActivity(new Intent(profileActivity.this, profileActivity.class));
+                                break;
+                            case R.id.settings_item:
+                                startActivity(new Intent(profileActivity.this, AccountActivity.class));
+                                break;
+                            case R.id.home_item:
+                                startActivity(new Intent(profileActivity.this, MainActivity.class));
+                                break;
+                            case R.id.scheduled_item:
+                                startActivity(new Intent(profileActivity.this, SessionsScheduledActivity.class));
+                                break;
+                            case R.id.requested_item:
+                                startActivity(new Intent(profileActivity.this, SessionRequestsActivity.class));
+                                break;
+                        }
+                        return true;
+                    }
+                });
+
     }
 }
