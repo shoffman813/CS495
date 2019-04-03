57bc3aaedc4d77434595b96af807b04d230c3c8b
shoffman2 <shoffman2@crimson.ua.edu>
2019-04-03 17:17:22 -0500

    added bottom nav to requested and scheduled sessions

--- app/src/main/java/com/example/tutorly/SessionRequestsActivity.java
+++ app/src/main/java/com/example/tutorly/SessionRequestsActivity.java
@@ -1,13 +1,44 @@
 package com.example.tutorly;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SessionRequestsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sessions);
+        setContentView(R.layout.activity_session_requests);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.profile_item:
+                                startActivity(new Intent(SessionRequestsActivity.this, profileActivity.class));
+                                break;
+                            case R.id.settings_item:
+                                startActivity(new Intent(SessionRequestsActivity.this, AccountActivity.class));
+                                break;
+                            case R.id.home_item:
+                                startActivity(new Intent(SessionRequestsActivity.this, MainActivity.class));
+                                break;
+                            case R.id.scheduled_item:
+                                startActivity(new Intent(SessionRequestsActivity.this, SessionsScheduledActivity.class));
+                                break;
+                            case R.id.requested_item:
+                                startActivity(new Intent(SessionRequestsActivity.this, SessionRequestsActivity.class));
+                                break;
+                        }
+                        return true;
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/tutorly/SessionsScheduledActivity.java
+++ app/src/main/java/com/example/tutorly/SessionsScheduledActivity.java
@@ -1,7 +1,11 @@
 package com.example.tutorly;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SessionsScheduledActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class SessionsScheduledActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sessions_scheduled);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.profile_item:
+                                startActivity(new Intent(SessionsScheduledActivity.this, profileActivity.class));
+                                break;
+                            case R.id.settings_item:
+                                startActivity(new Intent(SessionsScheduledActivity.this, AccountActivity.class));
+                                break;
+                            case R.id.home_item:
+                                startActivity(new Intent(SessionsScheduledActivity.this, MainActivity.class));
+                                break;
+                            case R.id.scheduled_item:
+                                startActivity(new Intent(SessionsScheduledActivity.this, SessionsScheduledActivity.class));
+                                break;
+                            case R.id.requested_item:
+                                startActivity(new Intent(SessionsScheduledActivity.this, SessionRequestsActivity.class));
+                                break;
+                        }
+                        return true;
+                    }
+                });
     }
 }
