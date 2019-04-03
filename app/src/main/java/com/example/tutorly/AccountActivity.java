384be040959b03d7d6ee2293509956091755cb15
shoffman2 <shoffman2@crimson.ua.edu>
2019-04-03 12:40:46 -0500

    added bottom nav to account screen

--- app/src/main/java/com/example/tutorly/AccountActivity.java
+++ app/src/main/java/com/example/tutorly/AccountActivity.java
@@ -1,7 +1,11 @@
 package com.example.tutorly;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.profile_item:
+                                startActivity(new Intent(AccountActivity.this, profileActivity.class));
+                                break;
+                            case R.id.settings_item:
+                                startActivity(new Intent(AccountActivity.this, AccountActivity.class));
+                                break;
+                            case R.id.home_item:
+                                startActivity(new Intent(AccountActivity.this, MainActivity.class));
+                                break;
+                            case R.id.scheduled_item:
+                                startActivity(new Intent(AccountActivity.this, SessionsScheduledActivity.class));
+                                break;
+                            case R.id.requested_item:
+                                startActivity(new Intent(AccountActivity.this, SessionRequestsActivity.class));
+                                break;
+                        }
+                        return true;
+                    }
+                });
     }
 }
