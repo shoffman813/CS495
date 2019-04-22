8487f1f803832e61255810a1cf905c28d7be425c
shoffman2 <shoffman2@crimson.ua.edu>
2019-04-21 23:32:41 -0500

    adding recyclerview and cards to TutorSearchListActivity

---
+++ app/src/main/java/com/example/tutorly/TutorAdapter.java
@@ -0,0 +1,64 @@
+package com.example.tutorly;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TutorAdapter extends RecyclerView.Adapter<TutorAdapter.TutorViewHolder> {
+
+
+    private Context mCtx;
+    private List<Tutor> tutorList;
+
+
+    public TutorAdapter(Context mCtx, List<Tutor> tutorList) {
+        this.mCtx = mCtx;
+        this.tutorList = tutorList;
+    }
+
+    @NonNull
+    @Override
+    public TutorViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) { //Creates a view holder instance
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.activity_list_layout, null);
+        return new TutorViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TutorViewHolder tutorViewHolder, int i) { //Binds data to view holder
+        Tutor tutor = tutorList.get(i);
+
+        tutorViewHolder.textViewFullName.setText(tutor.getFullName());
+        tutorViewHolder.textViewBio.setText(tutor.getShortBio());
+        tutorViewHolder.textViewRate.setText("$20.00/hr"); //Come back later and change, add to Tutor class
+
+        //holder.imageView.setImageDrawable(mCtx.getResources().getDrawable()); have to populate from tutor profile image
+    }
+
+    @Override
+    public int getItemCount() { //Returns the size of the list
+        return tutorList.size();
+    }
+
+    class TutorViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView; //Tutor profile picture
+        TextView textViewFullName, textViewBio, textViewRate;
+
+        public TutorViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            textViewFullName = itemView.findViewById(R.id.textViewFullName);
+            textViewBio = itemView.findViewById(R.id.textViewBio);
+            textViewRate = itemView.findViewById(R.id.textViewRate);
+        }
+    }
+}
--- app/src/main/java/com/example/tutorly/TutorSearchListActivity.java
+++ app/src/main/java/com/example/tutorly/TutorSearchListActivity.java
@@ -2,12 +2,20 @@ package com.example.tutorly;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*The screen to hold the list of tutor search results*/
 public class TutorSearchListActivity extends AppCompatActivity {
 
     /*Declaring Variables*/
     String classCode, classNumber;
+    RecyclerView recyclerView;
+    TutorAdapter adapter;
+    List<Tutor> tutorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,35 @@ public class TutorSearchListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tutor_search_list);
 
         classCode = getIntent().getStringExtra("class_code");
-        classNumber= getIntent().getStringExtra("class_number");
+        classNumber = getIntent().getStringExtra("class_number");
+
+
+        tutorList = new ArrayList<>();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true); //For recycler view, not elements inside it
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        /*The following code is for testing*/
+        Tutor tutor1 = new Tutor();
+        tutor1.setShortBio("I'm a third year CS student with experience tutoring in Algebra I and II.");
+        tutor1.setFirstName("Sarah");
+        tutor1.setLastName("Jones");
+
+        tutorList.add(tutor1);
+
+        Tutor tutor2 = new Tutor();
+        tutor2.setShortBio("I'm a fourth year Marketing Major looking to tutor in English");
+        tutor2.setFirstName("Dave");
+        tutor2.setLastName("Johnson");
+
+        tutorList.add(tutor2);
+
+        /*End Testing Code*/
+
+        adapter = new TutorAdapter(this, tutorList);
+        recyclerView.setAdapter(adapter);
+
     }
+
 }
